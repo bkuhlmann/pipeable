24b897e35024f0b7ea17df979ce850963249de85
Brooke Kuhlmann <brooke@alchemists.io>
2024-04-03 14:14:38 -0600

    Refactored Definer terminology
    Necessary to clarify the terminology used and improve readability.
    
    Milestone: minor

--- lib/pipeable/definer.rb
+++ lib/pipeable/definer.rb
@@ -10,9 +10,9 @@ module Pipeable
 
     using Refinements::Array
 
-    def initialize steps = Steps::Container, pipe: Pipe
+    def initialize container = Steps::Container, pipe: Pipe
       super()
-      @steps = steps
+      @container = container
       @pipe = pipe
       @instance_module = Class.new(Module).new
     end
@@ -26,20 +26,20 @@ module Pipeable
 
     private
 
-    attr_reader :steps, :pipe, :instance_module
+    attr_reader :container, :pipe, :instance_module
 
-    def define_pipe local_pipe = pipe
+    def define_pipe pipeline = pipe
       instance_module.define_method :pipe do |input, *steps|
         steps.each { |step| steps.supplant step, method(step) if step.is_a? Symbol }
-        local_pipe.call(input, *steps)
+        pipeline.call(input, *steps)
       end
     end
 
     def define_steps
-      instance_module.class_exec steps do |container|
-        container.each_key do |name|
+      instance_module.class_exec container do |dependencies|
+        dependencies.each_key do |name|
           define_method name do |*positionals, **keywords, &block|
-            step = container[name]
+            step = dependencies[name]
             step.is_a?(Proc) ? step : step.new(*positionals, **keywords, &block)
           end
         end
