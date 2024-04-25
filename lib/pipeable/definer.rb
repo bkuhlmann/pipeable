603c40c5804634fac12eb4d267a9a0571f357a68
Brooke Kuhlmann <brooke@alchemists.io>
2024-04-25 11:23:32 -0600

    Refactored builder to avoid intermediary class
    Necessary to simplify the implementation since the dynamic methods can be generated upon initialization rather than inclusion for the same effect. This reduces the lines of code and cleans up a Reek violation.
    
    Milestone: patch

--- lib/pipeable/definer.rb
+++ lib/pipeable/definer.rb
@@ -14,34 +14,27 @@ module Pipeable
       super()
       @container = container
       @pipe = pipe
-      @instance_module = Class.new(Module).new
-    end
 
-    def included descendant
-      super
       define_pipe
       define_steps
-      descendant.include instance_module
     end
 
     private
 
-    attr_reader :container, :pipe, :instance_module
+    attr_reader :container, :pipe
 
     def define_pipe pipeline = pipe
-      instance_module.define_method :pipe do |input, *steps|
+      define_method :pipe do |input, *steps|
         steps.each { |step| steps.supplant step, method(step) if step.is_a? Symbol }
         pipeline.call(input, *steps)
       end
     end
 
-    def define_steps
-      instance_module.class_exec container do |dependencies|
-        dependencies.each_key do |name|
-          define_method name do |*positionals, **keywords, &block|
-            step = dependencies[name]
-            step.is_a?(Proc) ? step : step.new(*positionals, **keywords, &block)
-          end
+    def define_steps vessel = container
+      vessel.each_key do |key|
+        define_method key do |*positionals, **keywords, &block|
+          step = vessel[key]
+          step.is_a?(Proc) ? step : step.new(*positionals, **keywords, &block)
         end
       end
     end
