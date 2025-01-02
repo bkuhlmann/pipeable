d820c96b8b936dd5cf35949b4ce0c3c5f0973ee2
Brooke Kuhlmann <brooke@alchemists.io>
2025-01-02 15:58:32 -0700

    Updated insert step to always append elements
    Necessary to reduce complexity by matching `merge` step behavior where only a single element or multiple elements are inserted into the array. If the object is not an array to begin with, it'll be cast as one.
    
    Milestone: minor

--- spec/lib/pipeable/steps/insert_spec.rb
+++ spec/lib/pipeable/steps/insert_spec.rb
@@ -5,49 +5,64 @@ require "spec_helper"
 RSpec.describe Pipeable::Steps::Insert do
   include Dry::Monads[:result]
 
-  subject(:step) { described_class.new :z }
-
   describe "#call" do
-    it "answers success with single element prepended to single input" do
+    it "answers success with single element appended to single element" do
+      step = described_class.new :z
+      expect(step.call(Success(:a)).success).to eq(%i[a z])
+    end
+
+    it "answers success with multiple elements appended to single element" do
+      step = described_class.new :x, :y, :z
+      expect(step.call(Success(:a)).success).to eq(%i[a x y z])
+    end
+
+    it "answers success with single element appended to array" do
+      step = described_class.new :z
+      expect(step.call(Success(%i[a b])).success).to eq(%i[a b z])
+    end
+
+    it "answers success with multiple elements appended to array" do
+      step = described_class.new :x, :y, :z
+      expect(step.call(Success(%i[a b])).success).to eq(%i[a b x y z])
+    end
+
+    it "answers success with single element prepended to single element" do
       step = described_class.new :z, at: 0
       expect(step.call(Success(:a)).success).to eq(%i[z a])
     end
 
-    it "answers success with single element prepended to input array" do
+    it "answers success with multiple elements prepended to single element" do
+      step = described_class.new :z, :y, :x, at: 0
+      expect(step.call(Success(:a)).success).to eq(%i[z y x a])
+    end
+
+    it "answers success with single element prepended to array" do
       step = described_class.new :z, at: 0
-      expect(step.call(Success(%i[a b])).success).to eq(%i[z a b])
+      expect(step.call(Success([:a])).success).to eq(%i[z a])
     end
 
-    it "answers success with array prepended to single input" do
-      step = described_class.new %i[y z], at: 0
-      expect(step.call(Success(:a)).success).to eq(%i[y z a])
+    it "answers success with multiple elements prepended to array" do
+      step = described_class.new :z, :y, :x, at: 0
+      expect(step.call(Success([:a])).success).to eq(%i[z y x a])
     end
 
-    it "answers success with single element inserted within input array" do
-      step = described_class.new :z, at: 2
-      expect(step.call(Success(%i[a b c])).success).to eq(%i[a b z c])
+    it "answers success with single element inserted within array" do
+      step = described_class.new :x, at: 2
+      expect(step.call(Success(%i[a b c])).success).to eq(%i[a b x c])
     end
 
-    it "answers success with array inserted within input array" do
-      step = described_class.new %i[y z], at: 2
+    it "answers success with multiple elements inserted within array" do
+      step = described_class.new :y, :z, at: 2
       expect(step.call(Success(%i[a b c])).success).to eq(%i[a b y z c])
     end
 
-    it "answers success with single element appended to single input" do
-      expect(step.call(Success(:a)).success).to eq(%i[a z])
-    end
-
-    it "answers success with single element appended to input array" do
-      expect(step.call(Success(%i[a b])).success).to eq(%i[a b z])
-    end
-
-    it "answers success with array appended to single input" do
-      step = described_class.new %i[y z]
-      expect(step.call(Success(:a)).success).to eq(%i[a y z])
+    it "answers success with array inserted" do
+      step = described_class.new [1, 2, 3]
+      expect(step.call(Success(:a)).success).to eq([:a, [1, 2, 3]])
     end
 
     it "answers success with hash inserted" do
-      step = described_class.new b: 2
+      step = described_class.new({b: 2})
       expect(step.call(Success(:a)).success).to eq([:a, {b: 2}])
     end
 
